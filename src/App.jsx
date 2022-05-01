cbd6cc55743fc28d74efadd1b123dc2a4609bdbf
wesleyafc <wesleyafc2010@gmail.com>
2022-05-01 10:36:46 -0300

    initial

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+
+
+    <div className="App">
+
+      <h1>where is my money</h1>
+      <footer>
+        <h1>version: {process.env.REACT_APP_VERSION}</h1>
+      </footer>
+    </div>
+
+
+  );
+}
+
+export default App;
